9abdda9f8ed1563c00400fdd23aa810d63604c86
Louis CALVIE <lcalvie@e2r10p6.clusters.42paris.fr>
2022-02-12 20:38:50 +0100

    dossier bonus

---
+++ alcu/header/alcu.h
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   alcu.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lcalvie <lcalvie@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/11 21:32:34 by agautier          #+#    #+#             */
+/*   Updated: 2022/02/12 19:40:21 by lcalvie          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ALCU_H
+#define ALCU_H
+
+#include <unistd.h>
+#include <fcntl.h>
+
+typedef struct s_list t_list;
+
+struct s_list
+{
+	int nbr;
+	t_list *next;
+	t_list *prev;
+};
+
+// MAP
+int	fill_board(int fd, t_list **board);
+
+// AI
+void ai_plays(t_list **board);
+
+// Player
+void player_plays(t_list **board, int fd);
+
+// Board
+void board_print(t_list *board);
+
+// Utils
+size_t ft_strlen(char *str);
+void ft_putstr_fd(int fd, char *str);
+void ft_putendl_fd(int fd, char *str);
+void ft_putnbr(int nbr);
+int ft_atoi(char *str);
+char *get_next_line(int fd);
+
+// List utils
+void list_push_back(t_list **list, int nbr);
+void list_pop_back(t_list **list);
+void list_clear(t_list **list);
+t_list *list_last(t_list *list);
+int	list_max(t_list *list);
+
+#endif
---
+++ alcu_bonus/header/get_next_line.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plefevre <plefevre@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/02 10:32:53 by plefevre          #+#    #+#             */
+/*   Updated: 2022/02/12 15:54:18 by agautier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFFER_SIZE 1024
+# include <unistd.h>
+# include <stdlib.h>
+
+typedef struct s_listbuf
+{
+	int					fd;
+	int					p;
+	int					res;
+	char				buff[BUFFER_SIZE];
+	struct s_listbuf	*next;
+}						t_list_buf;
+
+char		*get_next_line(int fd);
+char		*prolonge_malloc(char *s, int size);
+t_list_buf	*lst_new(int fd);
+t_list_buf	*lst_search(t_list_buf **list, int fd);
+void		lst_remove(t_list_buf **list, int fd);
+
+#endif
